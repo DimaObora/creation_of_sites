4f13969c61cf6d8a4ded8d6c44df298d4b91cd3d
DimaObora <obora.dima@yandex.ru>
2019-07-14 15:08:48 +0300

    тестировал балун

--- js/main.js
+++ js/main.js
@@ -23,39 +23,29 @@ $(document).ready(function ($) {
         });
         var placemark = new ymaps.Placemark(myMap.getCenter(), {
             // Зададим содержимое заголовка балуна.
-            balloonContentHeader: '<span class="description text-center">веб студия</span>' + '<a href = "#"> originals</a><br>',
+            balloonContentHeader:  '<div class="content_map"> ' +'<span class="description text-center">Веб студия</span>' + '<a href = "#"> Originals</a><br>',
             // Зададим содержимое основной части балуна.
-            balloonContentBody: '<img src="img/housJPG.JPG" height="150" width="200"> <br/> ' +
-                '<a href="tel:+79885242737"">+7(988)52-42-737&nbsp;&nbsp;&nbsp;</a>'+'<a class="tel" href="tel:+79002475139">+7(900) 24-75-139</a>',
-            // Зададим содержимое нижней части балуна.
-            balloonContentFooter: 'Старомышастовская  ул. Садовая, 211 ',
-            // Зададим содержимое всплывающей подсказки.
-            hintContent: 'Рога и копыта'
+            balloonContentBody:
+                '<img src="img/housJPG.JPG" height="150" width="200"> <br/> ' +
+                '<table border="0" width="100%" cellpadding="5">' +
+                '<tr>' +
+                '<td>Старомышастовская</td>' +
+                '<td><a href="tel:+79885242737">+7(988)52-42-737;</a></td>' +
+                '</tr>'+'<tr>' +
+                '<td> ул. Садовая 211 </td>' +
+                '<td><a class="tel" href="tel:+79002475139">+7(900) 24-75-139</a></td>' +
+                ' </tr>'+
+                `<tr><td colspan="2"><a class="mail text-center" href="mailto:web-originals@yandex.ru">web-originals@yandex.ru</a></td></tr>` +
+                `</table>`+
+                `</div>`,
         });
         // Добавим метку на карту.
         myMap.geoObjects.add(placemark);
         // Откроем балун на метке.
         placemark.balloon.open();
-        // var html = '<div class="popup">';
-        // html += '<img src="img/housJPG.jpg" alt="" />';
-        // html += '<div class="popup-text">';
-        // html += '<p>Старомышастовская <br> ул. Садовая, 211 <br> <a class="tel" href="tel:+79885242737">+79885242737</a>&nbsp;&nbsp;&nbsp;<a class="tel" href="tel:+79002475139">+79002475139</a></p>';
-        // html += '</div>';
-        // html += '</div>';
-
-        // var myPlacemark = new ymaps.Placemark([45.350938, 39.058148], {
-        //         balloonContent: html
-        //     },
-        //     {
-        //         iconLayout: 'default#image',
-        //         iconImageHref: 'img/icon.png',
-        //         iconImageSize: [50, 50],
-        //         iconImageOffset: [-33, -50],
-        //         balloonShadow: false
-        //     });
-
 
         myMap.geoObjects.add(myPlacemark);
+
 
     }
 
