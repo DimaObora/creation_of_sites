52963a124a16bb850c147d1ddfc957c7a071c98a
DimaObora <obora.dima@yandex.ru>
2019-07-18 10:41:23 +0300

    создал якорь для заявки, убрал кнопку все работы , добавил ссылку на телефон в разделе о нас

--- js/main.js
+++ js/main.js
@@ -1,4 +1,39 @@
 $(document).ready(function ($) {
+
+    // собираем все якоря; устанавливаем время анимации и количество кадров
+    const anchors = [].slice.call(document.querySelectorAll('a[href*="#"]')),
+        animationTime = 1000,
+        framesCount = 60;
+
+    anchors.forEach(function(item) {
+        // каждому якорю присваиваем обработчик события
+        item.addEventListener('click', function(e) {
+            // убираем стандартное поведение
+            e.preventDefault();
+
+            // для каждого якоря берем соответствующий ему элемент и определяем его координату Y
+            let coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top;
+
+            // запускаем интервал, в котором
+            let scroller = setInterval(function() {
+                // считаем на сколько скроллить за 1 такт
+                let scrollBy = coordY / framesCount;
+
+                // если к-во пикселей для скролла за 1 такт больше расстояния до элемента
+                // и дно страницы не достигнуто
+                if(scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
+                    // то скроллим на к-во пикселей, которое соответствует одному такту
+                    window.scrollBy(0, scrollBy);
+                } else {
+                    // иначе добираемся до элемента и выходим из интервала
+                    window.scrollTo(0, coordY);
+                    clearInterval(scroller);
+                }
+                // время интервала равняется частному от времени анимации и к-ва кадров
+            }, animationTime / framesCount);
+        });
+    });
+
     $(".phone-mask").mask("+7 (999) 99-99-999");
     ymaps.ready(init);
 
@@ -22,6 +57,8 @@ $(document).ready(function ($) {
         myMap.controls.add("zoomControl", {
             position: {top: 15, left: 15}
         });
+
+
         var placemark = new ymaps.Placemark(myMap.getCenter(), {
             // Зададим содержимое заголовка балуна.
             balloonContentHeader:  '<div class="content_map"> ' +'<span class="description text-center">Веб студия</span>' + '<a href = "#"> Originals</a><br>',
@@ -39,15 +76,14 @@ $(document).ready(function ($) {
                 `<tr><td colspan="2"><a class="mail text-center" href="mailto:web-originals@yandex.ru">web-originals@yandex.ru</a></td></tr>` +
                 `</table>`+
                 `</div>`,
+
         });
         // Добавим метку на карту.
         myMap.geoObjects.add(placemark);
         // Откроем балун на метке.
         placemark.balloon.open();
 
-        myMap.geoObjects.add(myPlacemark);
-
-
     }
 
 });
+
