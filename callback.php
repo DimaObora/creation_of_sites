efea94dcd203fc501289af3ccf5c5d48eda5d188
ikor <igor_korenovsk@mail.ru>
2019-08-08 22:18:18 +0300

    добавил отправлку последней формы

---
+++ callback.php
@@ -0,0 +1,51 @@
+<?php
+require_once "settings/settingscallback.php";
+$sendto   = "web-originals@yandex.ru";
+$usermail = $_POST['email'];
+$username = $_POST['name'];
+$userphone = $_POST['phone'];
+$content  = nl2br($_POST['message']);
+// Формирование заголовка письма
+$subject  = "Новое сообщение";
+$headers  = "From: " . strip_tags($usermail) . "\r\n";
+$headers .= "Reply-To: ". strip_tags($usermail) . "\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/html;charset=utf-8 \r\n";
+// Формирование тела письма
+$msg  = "<html><body style='font-family:Arial,sans-serif;'>";
+$msg .= "<h2 style='font-weight:bold;border-bottom:1px dotted #ccc;'>Новое сообщение </h2>\r\n";
+$msg .= "<p><strong>Имя:</strong> ".$username."</p>\r\n";
+$msg .= "<p><strong>Номер телефона:</strong> ".$userphone."</p>\r\n";
+$msg .= "<p><strong>Почта:</strong> ".$usermail."</p>\r\n";
+$msg .= "<p><strong>Сообщение:</strong> ".$content."</p>\r\n";
+$msg .= "</body></html>";
+// отправка сообщения
+send($VK_IM_ID,"web-originals.ru"."\r\nИмя:".$username."\r\nНомер телефона:".$userphone."\r\nПочта:".$usermail."\r\nСообщение:".$content, $VK_ACCESS_TOCEN,$VK_SECRET);
+if(@mail($sendto, $subject, $msg, $headers)) {
+	echo "true";
+} else {
+	echo "false";
+}
+
+function send($id, $message,$access_tocken,$secret)
+{
+    $url = 'https://api.vk.com/method/messages.send';
+    $params = array(
+        'peer_id' => $id,    // Кому отправляем
+        'message' => $message,   // Что отправляем
+        'access_token' => $access_tocken,  // access_token можно вбить хардкодом, если работа будет идти из под одного юзера
+        'v'=>'5.38',
+    );
+    $sig = md5("/method/messages.send?".http_build_query($params).$secret);
+    $params['sig']= $sig;
+    // В $result вернется id отправленного сообщения
+    $result = file_get_contents($url, false, stream_context_create(array(
+        'http' => array(
+            'method' => 'POST',
+            'header' => 'Content-type: application/x-www-form-urlencoded',
+            'content' => http_build_query($params)
+        )
+    )));
+}
+
+?>
---
+++ js/callback.js
@@ -0,0 +1,62 @@
+//валидация мыла в модалке
+function validateEmail(email) {
+    var reg = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return true;
+    return reg.test(email);
+}
+$(document).ready(function ($) {
+//кнопка отправки заявки
+    $("#feedbackform").submit(function () {
+        return false;
+    });
+    $(".callback-bt-1").on("click", function () {
+        var thisform = $("#"+this.parentElement.id);
+        var emailval = thisform.find("input[name$='email']").val();
+        var namevl = thisform.find("input[name$='name']").val();
+        var phonevl = thisform.find("input[name$='phone']").val();
+        var msgval = thisform.find("textarea[name$='message']").val();
+        var msglen = msgval.length;
+        var mailvalid = validateEmail(emailval);
+
+        if (mailvalid == false) {
+            thisform.find("input[name$='email']").addClass("error");
+        }
+        else if (mailvalid == true) {
+            thisform.find("input[name$='email']").removeClass("error");
+        }
+        if (mailvalid == false) {
+            thisform.find("input[name$='name']").addClass("error");
+        }
+        else if (mailvalid == true) {
+            thisform.find("input[name$='name']").removeClass("error");
+        }
+        if (mailvalid == false) {
+            thisform.find("input[name$='phone']").addClass("error");
+        }
+        else if (mailvalid == true) {
+            thisform.find("input[name$='phone']").removeClass("error");
+        }
+        if (msglen < 4) {
+            thisform.find("textarea[name$='message']").addClass("error");
+        }
+        else if (msglen >= 4) {
+            thisform.find("textarea[name$='message']").removeClass("error");
+        }
+        alert(0);
+        if (mailvalid == true && msglen >= 1) {
+            alert(1);
+            $.ajax({
+                type: 'POST',
+                url: 'callback.php',
+                data: thisform.serialize(),
+                success: function (data) {
+                    alert("Отправленно!");
+                },
+                error: function (data) {
+                    alert("Ошибка!");
+                    console.log(data);
+                }
+            });
+        }
+    });
+});
