e8ccfa0705913ad51b65ab7bec228fdf3e5ddaac
ikor <igor_korenovsk@mail.ru>
2019-08-09 09:04:57 +0300

    добавил композер с вкапи и пхпмайлер

--- callback.php
+++ callback.php
@@ -1,51 +1,61 @@
 <?php
+
+use VK\Client\VKApiClient;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require __DIR__ . '/vendor/autoload.php';
 require_once "settings/settingscallback.php";
+
 $usermail = $_POST['email'];
 $username = $_POST['name'];
 $userphone = $_POST['phone'];
 $content  = nl2br($_POST['message']);
-// Формирование заголовка письма
-$subject  = "Новое сообщение";
-$headers  = "From: " . strip_tags($usermail) . "\r\n";
-$headers .= "Reply-To: ". strip_tags($usermail) . "\r\n";
-$headers .= "MIME-Version: 1.0\r\n";
-$headers .= "Content-Type: text/html;charset=utf-8 \r\n";
-// Формирование тела письма
-$msg  = "<html><body style='font-family:Arial,sans-serif;'>";
-$msg .= "<h2 style='font-weight:bold;border-bottom:1px dotted #ccc;'>Новое сообщение </h2>\r\n";
-$msg .= "<p><strong>Имя:</strong> ".$username."</p>\r\n";
-$msg .= "<p><strong>Номер телефона:</strong> ".$userphone."</p>\r\n";
-$msg .= "<p><strong>Почта:</strong> ".$usermail."</p>\r\n";
-$msg .= "<p><strong>Сообщение:</strong> ".$content."</p>\r\n";
-$msg .= "</body></html>";
+
+
 // отправка сообщения
-send($VK_IM_ID,"web-originals.ru"."\r\nИмя:".$username."\r\nНомер телефона:".$userphone."\r\nПочта:".$usermail."\r\nСообщение:".$content, $VK_ACCESS_TOCEN,$VK_SECRET);
-if(@mail($sendto, $subject, $msg, $headers)) {
-	echo "true";
-} else {
-	echo "false";
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 2;                                       // Enable verbose debug output
+    $mail->CharSet =  "utf-8";
+    $mail->isSMTP();                                            // Set mailer to use SMTP
+    $mail->Host       = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = $EMAIL_LOGIN;                     // SMTP username
+    $mail->Password   = $EMAIL_PASSWORD;                               // SMTP password
+    $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 587;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom($EMAIL_LOGIN, 'Web-Originals');
+    $mail->addAddress($SEND_TO_EMAIL, 'username');     // Add a recipient
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Новое сообщение';
+    // Формирование тела письма
+    $msg  = "<html><body style='font-family:Arial,sans-serif;'>";
+    $msg .= "<h2 style='font-weight:bold;border-bottom:1px dotted #ccc;'>Новое сообщение </h2>\r\n";
+    $msg .= "<p><strong>Имя:</strong> ".$username."</p>\r\n";
+    $msg .= "<p><strong>Номер телефона:</strong> ".$userphone."</p>\r\n";
+    $msg .= "<p><strong>Почта:</strong> ".$usermail."</p>\r\n";
+    $msg .= "<p><strong>Сообщение:</strong> ".$content."</p>\r\n";
+    $msg .= "</body></html>";
+    $mail->Body    = $msg;
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
 
-function send($id, $message,$access_tocken)
-{
-    $url = 'https://api.vk.com/method/messages.send';
-    $params = array(
-        'user_ids' => $id,    // Кому отправляем
-        'message' => $message,   // Что отправляем
-        'access_token' => $access_tocken,  // access_token можно вбить хардкодом, если работа будет идти из под одного юзера
-        'v'=>'5.38',
-    );
-    $sig = md5("/method/messages.send?".http_build_query($params));
-    $params['sig']= $sig;
-    // В $result вернется id отправленного сообщения
-    $result = file_get_contents($url, false, stream_context_create(array(
-        'http' => array(
-            'method' => 'POST',
-            'header' => 'Content-type: application/x-www-form-urlencoded',
-            'content' => http_build_query($params)
-        )
-    )));
-    echo $result;
-}
+$vk = new VKApiClient();
+$vk->messages()->send($VK_ACCESS_TOCEN, $params = array(
+    'user_ids' => $VK_IM_ID,    // Кому отправляем
+    'message' => "web-originals.ru"."\r\nИмя:".$username."\r\nНомер телефона:".$userphone."\r\nПочта:".$usermail."\r\nСообщение:".$content,   // Что отправляем);
+    'random_id' => rand(10000,100000000),
+));
 
-?>
