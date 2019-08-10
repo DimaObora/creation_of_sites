ac70eeb65255ae12d6290c4a0b0dec2c2143b136
ikor <igor_korenovsk@mail.ru>
2019-08-10 10:13:46 +0300

    добавил проверку полей

--- js/callback.js
+++ js/callback.js
@@ -4,37 +4,50 @@ function validateEmail(email) {
     return true;
     return reg.test(email);
 }
+
 $(document).ready(function ($) {
 //кнопка отправки заявки
     $(".nosumbit").submit(function () {
         return false;
     });
     $(".callback-bt-1").on("click", function () {
-        var thisform = $("#"+this.parentElement.id);
-        var emailval = thisform.find("input[name$='email']").val();
-        var namevl = thisform.find("input[name$='name']").val();
-        var phonevl = thisform.find("input[name$='phone']").val();
-        var msgval = thisform.find("textarea[name$='message']").val();
-        var mailvalid = validateEmail(emailval);
-
-        if (mailvalid == true ) {
-            console.log(thisform.serialize());
-            $.ajax({
-                type: 'POST',
-                url: 'callback.php',
-                data: thisform.serialize(),
-                success: function (data) {
-                    alert("Отправленно!");
-                    console.log(data);
-                },
-                error: function (data) {
-                    alert("Ошибка!");
-                    console.log(data);
+        var thisform = $("#" + this.parentElement.id);
+        var st = thisform.serialize().split('&');
+        var badfields = false;
+        st.forEach(function (value){
+            var valuesplit = value.split('=');
+            if(valuesplit[1] == ''){
+                badfields = true;
+                if(valuesplit[0] == "g-recaptcha-response"){
+                    alert("Необходимо пройти капчу");
+                }else{
+                    thisform.find("input[name$='"+valuesplit[0]+"']").addClass("error");
                 }
-            }).done(function (data) {
-                console.log(data);
-            });
+            }else{
+                thisform.find("input[name$='"+valuesplit[0]+"']").removeClass("error");
+            }
+        });
+        if(badfields){
+            grecaptcha.reset();
+            return;
         }
+        console.log();
+        $.ajax({
+            type: 'POST',
+            url: 'callback.php',
+            data: thisform.serialize(),
+            success: function (data) {
+                alert("Отправленно!");
+                console.log(data);
+            },
+            error: function (data) {
+                alert("Ошибка!");
+                console.log(data);
+            }
+        }).done(function (data) {
+            console.log(data);
+        });
+
         grecaptcha.reset();
     });
 });
